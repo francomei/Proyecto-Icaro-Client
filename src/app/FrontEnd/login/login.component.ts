cc22c1e5bd9f291628972d68ab42a228f8fd7c42
unknown <francomeinardo13@gmail.com>
2022-10-11 10:37:53 -0300

    .

--- src/app/FrontEnd/login/login.component.ts
+++ src/app/FrontEnd/login/login.component.ts
@@ -43,7 +43,6 @@ hide = true
 
   registerUser(){
     const user = {username: this.miformulario.value.username, firstname: this.miformulario.value.firstname, lastname: this.miformulario.value.lastname, password:this.miformulario.value.password, country:this.miformulario.value.country, city:this.miformulario.value.city};
-    // console.log(this.miformulario.value)
     this.userService.login(user).subscribe(data =>{
       this.userService.setToken(data.token);
       this.router.navigateByUrl('/principal');
